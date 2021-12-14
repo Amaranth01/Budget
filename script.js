5229df6cd58323dfd02add7b1205792dff6fdbe9
Amaranth01 <vanessa.nectoux@gmail.com>
2021-12-14 13:13:57 +0100

    step 3

--- script.js
+++ script.js
@@ -3,40 +3,47 @@ let take = document.querySelector('#take').getElementsByTagName('input');
 let sapes = document.querySelector('#sapes').getElementsByTagName('input');
 let buttonResult = document.querySelector('#resultButton');
 let result = document.querySelector('#result');
-let phrase = document.querySelector("#phase")
+let phrase = document.getElementById('phrase');
 let buttonReset = document.querySelector('#reset');
-
-//Variable for the base of the result
-
-let resultExpend = 0;
-let resultTake = 0;
-let sapesResult = 0;
-
-//a for loop to calculate each result of div
-for (let expend of expenditures) {
-    resultExpend = (parseFloat(expend.value));
-}
-
-for (let taking of take) {
-    resultTake = (parseFloat(taking.value));
-}
-
-for (let sapping of sapes) {
-    sapesResult = (parseFloat(sapping.value));
-}
 
 let resultTotal;
 
-
 //initialize button for calculate the final result
-buttonResult.addEventListener('click',function () {
-    result = ((resultTake) - (resultExpend) - (sapesResult));
-    result.innerHTML = result.value;
-    console.log(result)
-    if (result > '0') {
-        phrase.innerHTML = 'Félicitations ! Vous pouvez vous acheter un jeu, ou une nouvelle voiture !';
+buttonResult.addEventListener('click', function () {
+
+    //Variable for the base of the result
+    let resultExpend = 0;
+    let resultTake = 0;
+    let sapesResult = 0;
+
+    //a for loop to calculate each result of div
+    for (let expend of expenditures) {
+        resultExpend += (parseFloat(expend.value));
+        console.log(expend.value);
     }
-    // else {
-    //     phrase.innerHTML = 'Il va falloir penser à manger des pâtes...';
-    // }
+
+    for (let taking of take) {
+        resultTake += (parseFloat(taking.value));
+    }
+
+    for (let sapping of sapes) {
+        sapesResult += (parseFloat(sapping.value));
+    }
+
+    resultTotal = resultTake  - resultExpend- sapesResult;
+    console.log(resultTotal);
+    result.innerHTML = resultTotal;
+    console.log(resultTake, resultExpend, sapesResult);
+
+    if (resultTotal > 0) {
+        phrase.innerHTML = 'Félicitations ! Vous pouvez vous acheter un jeu, ou une nouvelle voiture !';
+        phrase.style.color = "darkgreen";
+        phrase.style.fontWeight = "bold";
+    } else {
+        phrase.innerHTML = 'Il va falloir penser à manger des pâtes...';
+        phrase.style.color = 'red';
+        phrase.style.fontWeight = 'bold';
+    }
+
+
 })
